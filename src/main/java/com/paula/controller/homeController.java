1dfc4b8b7fcf9be6c11c3bdb34d91e06ec385b66
Paulamf23 <122610982+Paulamf23@users.noreply.github.com>
2024-04-16 14:44:35 +0200

    login y registrar

--- src/main/java/com/paula/controller/homeController.java
+++ src/main/java/com/paula/controller/homeController.java
@@ -1,16 +1,20 @@
 package com.paula.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/general")
 public class homeController {
-    
-    @GetMapping("")  
-    public String home(){
+
+    @GetMapping("")
+    public String home() {
         return "general/home";
+    }
+
+    @GetMapping("/login") // Agregamos un nuevo método para manejar la solicitud de la página de inicio de sesión
+    public String loginPage() {
+        return "general/login";
     }
 }
