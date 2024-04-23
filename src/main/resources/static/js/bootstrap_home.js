c7d0e68192beb1f6384f97e7a59b5961710e1f19
Paulamf23 <122610982+Paulamf23@users.noreply.github.com>
2024-04-23 08:49:45 +0200

    login style terminado

--- src/main/resources/static/js/bootstrap_home.js
+++ src/main/resources/static/js/bootstrap_home.js
@@ -1,28 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var dropdownToggle = document.getElementById('filtroGeneroBtn');
-  dropdownToggle.addEventListener('click', function () {
-    var dropdownMenu = dropdownToggle.nextElementSibling;
-    if (dropdownMenu.classList.contains('show')) {
-      dropdownMenu.classList.remove('show');
-    } else {
-      dropdownMenu.classList.add('show');
-    }
-  });
-});
-
-
-let x = document.getElementById('login');
-let y = document.getElementById('register');
-let z = document.getElementById('btn');
-
-function register() {
-  x.style.left = '-400px';
-  y.style.left = '50px';
-  z.style.left = '110px';
+function showRegisterForm() {
+    document.querySelector('.container-form').classList.add('hidden');
+    document.querySelectorAll('.container-form')[1].classList.remove('hidden');
 }
 
-function login() {
-  x.style.left = '50px';
-  y.style.left = '450px';
-  z.style.left = '0px';
+function showLoginForm() {
+    document.querySelectorAll('.container-form')[1].classList.add('hidden');
+    document.querySelector('.container-form').classList.remove('hidden');
 }
