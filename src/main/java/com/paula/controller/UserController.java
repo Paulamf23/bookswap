7783598ccc6cc0d497226ee31f4f71659b7c54d1
Paulamf23 <122610982+Paulamf23@users.noreply.github.com>
2024-04-29 10:52:18 +0200

    Correcion rutas

--- src/main/java/com/paula/controller/UserController.java
+++ src/main/java/com/paula/controller/UserController.java
@@ -91,12 +91,11 @@ public class UserController {
                     session.setAttribute("email", user.getEmail());
                     session.setAttribute("name", user.getName());
                     session.setAttribute("username", user.getUsername());
-                    return "/home";
+                    return "home";
                 }
             }
             redirectAttributes.addFlashAttribute("errorUsuarioExiste", "El nombre de usuario ya existe.");
             return "register";
         }
     }
-
 }
