91245498196b1258cec3859a37ffe2974b0f4ddc
Paulamf23 <122610982+Paulamf23@users.noreply.github.com>
2024-05-03 12:34:23 +0200

    Cambio perfil 2

--- src/main/java/com/paula/controller/UserController.java
+++ src/main/java/com/paula/controller/UserController.java
@@ -41,32 +41,38 @@ public class UserController {
 
     // // Error aqui!!!!
     // @PostMapping("/loginUser")
-    // public String login(@Valid @ModelAttribute User user, BindingResult bindingResult, HttpSession hSession,
-    //         Model model, RedirectAttributes redirect) {
-    //     if (hSession.getAttribute("email") != null) {
-    //         return "redirect:/home";
-    //     }
-    
-    //     if (bindingResult.hasErrors()) {
-    //         return "login";
-    //     } else {
-    //         User userFind = userService.getUser(user.getEmail());
-    //         if (userFind != null && Encriptation.validatePassword(user.getPassword(), userFind.getPassword())) {
-    //             hSession.setAttribute("email", userFind.getEmail());
-    //             return "redirect:/home"; 
-    //         } else {
-    //             redirect.addFlashAttribute("errorUsuarioNoExiste", "Usuario o contraseña incorrectos.");
-    //             return "redirect:/login";
-    //         }
-    //     }
+    // public String login(@Valid @ModelAttribute User user, BindingResult
+    // bindingResult, HttpSession hSession,
+    // Model model, RedirectAttributes redirect) {
+    // if (hSession.getAttribute("email") != null) {
+    // return "redirect:/home";
     // }
-    
+
+    // if (bindingResult.hasErrors()) {
+    // return "login";
+    // } else {
+    // User userFind = userService.getUser(user.getEmail());
+    // if (userFind != null && Encriptation.validatePassword(user.getPassword(),
+    // userFind.getPassword())) {
+    // hSession.setAttribute("email", userFind.getEmail());
+    // return "redirect:/home";
+    // } else {
+    // redirect.addFlashAttribute("errorUsuarioNoExiste", "Usuario o contraseña
+    // incorrectos.");
+    // return "redirect:/login";
+    // }
+    // }
+    // }
+
     @GetMapping("/perfil")
-    public String perfilPage(HttpSession hSession) {
+    public String perfilPage(HttpSession hSession, Model model) {
         if (hSession.getAttribute("email") != null) {
             String email = hSession.getAttribute("email").toString();
             User user = userService.getUser(email);
             hSession.setAttribute("userInfo", user);
+
+            model.addAttribute("username", user.getUsername());
+
             return "perfil";
         }
 
